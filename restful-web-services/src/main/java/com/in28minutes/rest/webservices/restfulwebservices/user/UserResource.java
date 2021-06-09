ebd5d7694d04f9f40af6bd8fe1e94cb101b62cab
August Marsalis <aj@Augusts-MacBook-Pro.local>
2021-06-09 15:15:05 -0500

    Created a simple resource to accept a post

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +28,16 @@ public class UserResource {
 	public User retrieveUser(@PathVariable int id) {
 		return service.findeOne(id);
 	}
-	//retreiveUser(int id)
+	
+	//CREATED
+	//input - details of a user
+	//output - created & return the created the URI
+	
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		
+	}
+	
 
 }
