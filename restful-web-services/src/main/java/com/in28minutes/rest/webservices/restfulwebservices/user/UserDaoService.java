d0f1c22bace1847d214659ed643164c478c40a29
August Marsalis <aj@Augusts-MacBook-Pro.local>
2021-06-09 12:24:57 -0500

    Defined simple user bean with id, name and birthday. Created methods to play around with users. to save, findone and return a list of users

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,14 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.ArrayList;
+
+import org.springframework.context.support.StaticApplicationContext;
+import org.springframework.stereotype.Component;
+
+import antlr.collections.List;
+
+@Component
+public class UserDaoService {
+ private static List<User> users = new ArrayList<>();
+ 
+}
